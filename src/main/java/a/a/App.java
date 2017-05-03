ea5a03e642896b9650d0dea2ef5c957ab35a276b
Mahender Muthyala <mahender@newtglobal.com>
2017-05-03 15:21:12 +0530

    webhook example

---
+++ src/main/java/a/a/App.java
@@ -0,0 +1,14 @@
+package a.a;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Wel come to my world...");
+    }
+}
